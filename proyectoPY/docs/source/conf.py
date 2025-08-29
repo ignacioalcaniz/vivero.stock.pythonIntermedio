78d98f22ae3e5db5e6f8f65a7987204d8ff5b811
ignacio alcaniz <ignaalcaniz@gmail.com>
2025-08-29 16:46:27 -0300

    proyecto intermedio terminado para entregar

--- proyectoPY/docs/source/conf.py
+++ proyectoPY/docs/source/conf.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../../../'))
 
 
@@ -23,7 +24,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 language = 'es'
 
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 
 
